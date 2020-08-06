29e7ce3b8960828c88c70c8af9ec2b9e1790ae7d
Baki-code <t.a.spiridonob@gmail.com>
2020-08-06 11:00:04 +0300

    HW6

--- src/HW6.java
+++ src/HW6.java
@@ -2,10 +2,21 @@ import com.sun.org.apache.xerces.internal.impl.dv.xs.AnyURIDV;
 
 public class HW6 {
     public static void main(String[] args) {
+        Animal cat1 = new Cat("Барсик");
+        Animal dog1 = new Dog("Тузик");
+        Animal cat2 = new Cat("Мурзик");
 
+        cat1.run(400);
+        cat1.swim(100);
+        cat1.run(100);
+        dog1.run(300);
+        dog1.run(1000);
+        dog1.swim(100);
+        dog1.swim(8);
 
-
-
+        Cat.printCount();
+        Dog.printCount();
+        Animal.printCount();
 
     }
 }
